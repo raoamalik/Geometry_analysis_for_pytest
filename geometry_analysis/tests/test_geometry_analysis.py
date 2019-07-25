28c9a91d3369d8b56af20a110a3cce7c453bf5d1
raoamalik <raoamalik@gmail.com>
2019-07-25 11:45:18 -0500

    created geometry analysis

--- geometry_analysis/tests/test_geometry_analysis.py
+++ geometry_analysis/tests/test_geometry_analysis.py
@@ -6,7 +6,27 @@ Unit and regression test for the geometry_analysis package.
 import geometry_analysis
 import pytest
 import sys
+import numpy as np
 
 def test_geometry_analysis_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "geometry_analysis" in sys.modules
+
+def test_calculate_distance():
+    """Test the calculate_distance function"""
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 1, 0])
+    expected_distance = np.sqrt(2.)
+    calculated_distance = geometry_analysis.calculate_distance(r1, r2)
+
+    assert expected_distance == calculated_distance 
+
+    def test_calculate_angle():
+        """Test the calculate_angle function"""
+        rA = np.array([0, 0, 1])
+        rB = np.array([0, 1, 0])
+        rC = np.array([1, 0, 0])
+        expected_angle = 90
+        calculated_angle = geometry_analysis.calculate_angle(rA, rB, rC)
+
+        assert expected_angle == calculated_angle
